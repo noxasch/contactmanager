4d6c19ef638e558e28093fef11646376761ca10d
Alexander Dischberg <noxasch@gmail.com>
2019-01-23 01:47:26 +0800

    Updates

---
+++ precache-manifest.4b3df0ee01765fc4826da385fa082f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "8ebb1e6571d45293e8c5",
+    "url": "/contactmanager/static/js/main.8ebb1e65.chunk.js"
+  },
+  {
+    "revision": "ad88b10fe7d202643818",
+    "url": "/contactmanager/static/js/1.ad88b10f.chunk.js"
+  },
+  {
+    "revision": "8ebb1e6571d45293e8c5",
+    "url": "/contactmanager/static/css/main.c6a48109.chunk.css"
+  },
+  {
+    "revision": "ad88b10fe7d202643818",
+    "url": "/contactmanager/static/css/1.c75509b8.chunk.css"
+  },
+  {
+    "revision": "69e36c7c8c92655b180d29e44fce94b9",
+    "url": "/contactmanager/index.html"
+  }
+];
